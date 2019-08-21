707ed301d30fc4cc457a249f0e11d1cbfc9ed7eb
Surya <surya3214@gmail.com>
2019-08-22 01:18:13 +0530

    Batch commit - 22/08/2019
    Signed-off-by: Surya <surya3214@gmail.com>

--- CodeChef/GPD.cpp
+++ CodeChef/GPD.cpp
@@ -1,10 +1,11 @@
-// NA
+// AC Persistent Trie
 #include <bits/stdc++.h>
 #define BUFFER 5
 #define BUFF(x) x + BUFFER
 #define N_MAX (int) 1e5
-#define H_MAX 31
-#define NODES_MAX (int) (N_MAX * H_MAX)
+#define Q_MAX (int) 2e5
+#define BITS_MAX 31
+#define NODES_MAX (int) ((N_MAX + Q_MAX) * BITS_MAX)
 // #pragma GCC optimize "O4"
 using namespace std;
 typedef long long int ll;
@@ -12,25 +13,25 @@ typedef unsigned long long int ull;
 typedef pair<int, int> pii;
 map<int, int> M;
 int root, c_ids;
-int powers[BUFF(H_MAX)];
+int powers[BUFF(BITS_MAX)];
 struct {
-  int key, parent, root;
+  int key, root;
   vector<int> adj;
-} stations[BUFF(N_MAX)];
+} stations[BUFF(N_MAX + Q_MAX)];
 struct {
   int l, r, cnt;
-} nodes_pool[BUFF(NODES_MAX)];
+} nodes[BUFF(NODES_MAX)];
 int nodes_ptr;
 int number_buffer[BUFF(BITS_MAX)];
 void fillBuffer(int x) {
-  for (int i = BITS_MAX; i >= 0; --i) {
+  for (int i = BITS_MAX; i; --i) {
     number_buffer[i] = x & 1;
     if (x)
       x = x >> 1;
   }
 }
 void invertBuffer() {
-  for (int i = BITS_MAX; i >= 0; --i)
+  for (int i = BITS_MAX; i; --i)
     number_buffer[i] = !number_buffer[i];
 }
 int insertIntoTrie(int prev) {
@@ -39,14 +40,17 @@ int insertIntoTrie(int prev) {
   nodes[next] = nodes[prev];
   cur = next;
   ++nodes[cur].cnt;
-  for (int i = 0; i <= BITS_MAX; ++i) {
+  for (int i = 1; i <= BITS_MAX; ++i) {
     next = ++nodes_ptr;
+    assert(nodes_ptr <= NODES_MAX);
     if (number_buffer[i]) {
-      nodes[next] = nodes[prev].r;
+      nodes[next] = nodes[nodes[prev].r];
       nodes[cur].r = next;
+      prev = nodes[prev].r;
     } else {
-      nodes[next] = nodes[prev].l;
+      nodes[next] = nodes[nodes[prev].l];
       nodes[cur].l = next;
+      prev = nodes[prev].l;
     }
     cur = next;
     ++nodes[cur].cnt;
@@ -54,7 +58,6 @@ int insertIntoTrie(int prev) {
   return ret;
 }
 void fillSingleNodeInfo(int cur, int prev) {
-  stations[cur].parent = prev;
   fillBuffer(stations[cur].key);
   stations[cur].root = insertIntoTrie(stations[prev].root);
 }
@@ -69,11 +72,22 @@ void process() {
 }
 int findClosest(int cur) {
   int ret = 0;
-  for (int i = 0, pos = 0; i <= BITS_MAX; ++i) {
+  bool left;
+  for (int i = 1; i <= BITS_MAX; ++i) {
     if (number_buffer[i]) {
-
+      if (nodes[nodes[cur].r].cnt)
+        left = false;
+      else left = true;
     } else {
-
+      if (nodes[nodes[cur].l].cnt)
+        left = true;
+      else left = false;
+    }
+    if (left)
+      cur = nodes[cur].l;
+    else {
+      ret += powers[i];
+      cur = nodes[cur].r;
     }
   }
   return ret;
@@ -86,37 +100,38 @@ void program() {
   stations[M[r]].key = key;
   for (int i = 1; i < n; ++i) {
     scanf("%d %d %d", &u, &v, &k);
-    if (!M[u]) {
-      M[u] = ++c_ids;
-      stations[M[u]].key = k;
-    } else if (!M[v]) {
-      M[v] = ++c_ids;
-      stations[M[v]].key = k;
-    }
+    M[u] = ++c_ids;
+    stations[M[u]].key = k;
     stations[M[u]].adj.push_back(M[v]);
     stations[M[v]].adj.push_back(M[u]);
   }
   process();
-  for (int i = 1, type, a, b; i <= q; ++i) {
+  for (int i = 1, ans = 0, type, a, b; i <= q; ++i) {
     scanf("%d", &type);
+    type ^= ans;
     if (!type) {
       scanf("%d %d %d", &v, &u, &k);
+      v ^= ans;
+      u ^= ans;
+      k ^= ans;
       M[u] = ++c_ids;
       stations[M[u]].key = k;
       fillSingleNodeInfo(M[u], M[v]);
     } else {
       scanf("%d %d", &v, &k);
+      v ^= ans;
+      k ^= ans;
       fillBuffer(k);
-      a = findClosest(stations[M[v]].root);
+      a = findClosest(stations[M[v]].root) ^ k;
       invertBuffer();
-      b = findClosest(stations[M[v]].root);
+      b = findClosest(stations[M[v]].root) ^ k;
       printf("%d %d\n", a, b);
+      ans = a ^ b;
     }
   }
 }
 void ready() {
-  int ret = 1;
-  for (int i = 0; i <= H_MAX; ++i, ret = ret << 1)
+  for (int i = BITS_MAX, ret = 1; i; --i, ret = ret << 1)
     powers[i] = ret;
 }
 int main() {
---
+++ CodeChef/PSHTTR.cpp
@@ -0,0 +1,117 @@
+// NA
+#include <bits/stdc++.h>
+#define BUFFER 5
+#define BUFF(x) x + BUFFER
+#define N_MAX (int) 1e5
+#define M_MAX (int) 1e5
+#define E_MAX (N_MAX + M_MAX)
+#define B_MAX (2 * E_MAX)
+// #pragma GCC optimize "O4"
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<int, int> pii;
+int n;
+struct {
+  int depth, start, end;
+  vector<int> adj;
+} castles[BUFF(N_MAX)];
+int t_time;
+struct E {
+  int u, v, k;
+  bool type;
+  E() {}
+  E(int u, int v, int k, bool type = false) : u(u), v(v), type(type) {}
+  bool operator <(E b) {
+    return ((k < b.k) || (!type && b.type));
+  }
+} events[BUFF(E_MAX)];
+int e_ptr;
+struct {
+  int time;
+  int val;
+} BIT[BUFF(B_MAX)];
+int b_time;
+void reset() {
+  e_ptr = t_time = 0;
+  ++b_time;
+  for (int i = 1; i <= N_MAX; ++i)
+    castles[i].adj.clear();
+}
+void dfs(int cur, int prev) {
+  castles[cur].depth = castles[prev].depth + 1;
+  castles[cur].start = ++t_time;
+  for (auto next: castles[cur].adj)
+    if (next != prev)
+      dfs(next, cur);
+  castles[cur].end = ++t_time;
+}
+void process() {
+  dfs(1, 0);
+  sort(events + 1, events + 1 + e_ptr);
+}
+void sanitizeBIT(int idx) {
+  if (BIT[idx].time != b_time) {
+    BIT[idx].time = b_time;
+    BIT[idx].val = 0;
+  }
+}
+void addToBIT(int val, int idx) {
+  while (idx <= t_time) {
+    sanitizeBIT(idx);
+    BIT[idx].val ^= val;
+    idx += (idx & -idx);
+  }
+}
+int queryBIT(int idx) {
+  int ret = 0;
+  while (idx) {
+    ret ^= BIT[idx].val;
+    idx -= (idx & -idx);
+  }
+  return ret;
+}
+void addEdge(int e_idx) {
+  int u, v;
+  u = events[e_idx].u, v = events[e_idx].v;
+  if (castles[v].depth < castles[u].depth)
+    swap(u, v);
+  addToBIT(events[e_idx].k, castles[v].start);
+  addToBIT(events[e_idx].k, castles[v].end);
+}
+void program() {
+  int tcase;
+  scanf("%d", &tcase);
+  for (int t = 1, u, v, k, m, ans; t <= tcase; ++t) {
+    reset();
+    scanf("%d", &n);
+    for (int i = 1; i < n; ++i) {
+      scanf("%d %d %d", &u, &v, &k);
+      castles[u].adj.push_back(v);
+      castles[v].adj.push_back(u);
+      events[++e_ptr] = E(u, v, k);
+    }
+    scanf("%d", &m);
+    for (int i = 1; i <= m; ++i) {
+      scanf("%d %d %d", &u, &v, &k);
+      events[++e_ptr] = E(u, v, k, true);
+    }
+    process();
+    for (int i = 1; i <= e_ptr; ++i) {
+      if (events[i].type) {
+        ans = queryBIT(castles[events[i].u].end);
+        ans ^= queryBIT(castles[events[i].u].start - 1);
+        ans ^= queryBIT(castles[events[i].v].end);
+        ans ^= queryBIT(castles[events[i].v].start - 1);
+        printf("%d\n", ans);
+      } else addEdge(i);
+    }
+  }
+}
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+#endif
+  program();
+  return 0;
+}
